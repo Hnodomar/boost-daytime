690faab92ecdf685723c5e82e54bf410250993be
Hnodomar <bluesilverg22@gmail.com>
2021-08-06 14:45:32 +0100

    basic boost daytime client implementatio

--- src/client.cpp
+++ src/client.cpp
@@ -13,5 +13,20 @@ int main(int argc, char* argv[]) {
         boost::asio::io_context io_context;
         tcp::resolver resolver(io_context);
         tcp::resolver::results_type endpoints = resolver.resolve(argv[1], "daytime");
+        tcp::socket socket(io_context);
+        boost::asio::connect(socket, endpoints);
+        for (;;) {
+            boost::array<char, 128> buf;
+            boost::system::error_code error;
+            size_t len = socket.read_some(boost::asio::buffer(buf), error);
+            if (error == boost::asio::error::eof)
+                break;
+            else if (error)
+                throw boost::system::system_error(error);
+            std::cout.write(buf.data(), len);
+        }
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
     }
 }
